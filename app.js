647e86a4c52d3fa348be16b2ceb06910f93c9f7e
Anna Karutina <anna.karutina@khk.ee>
2020-12-11 08:18:11 +0200

    Add task data to LS and get data form LS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('#tasks-list');
 
 // define event listeners
+// page reload event - get data from Local Storage
+document.addEventListener('DOMContentLoaded', getTasks);
 // add task to list - submit button
 form.addEventListener('submit', addTask);
 
@@ -12,6 +14,9 @@ function addTask(e) {
     const taskInput = document.querySelector('#task').value;
     // create new ui object
     const ui = new UI();
+    // create new Local Storage object
+    const ls = new LS();
+
     if (taskInput === '') {
         ui.alertMessage("Add task data!", "problem");
     } else {
@@ -19,8 +24,26 @@ function addTask(e) {
         const task = new Task(taskInput);
         // add task object data to html list
         ui.addTaskToTable(task);
+        // save task data to Local Storage
+        ls.saveTask(task);
         // show ok alert messege
         ui.alertMessage("Added task to todo-list!", "ok");
         e.preventDefault();
     }
+}
+
+// get tasks from Local Storage
+function getTasks() {
+    // create new Local Storage object
+    const ls = new LS();
+    // create new ui object
+    const ui = new UI();
+    // get tasks from LS
+    const tasks = ls.getTasks();
+    // get each task from LS and transform to Task object
+    tasks.forEach(function (task) {
+        const taskData = new Task(task['task']);
+        // create UI object for html list item
+        ui.addTaskToTable(taskData);
+    });
 }
